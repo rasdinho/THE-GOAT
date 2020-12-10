1f576cc3c108d1a179a9424f07fc2725e138bcf2
rasdinho <ras.dinho7@gmail.com>
2020-12-10 11:14:08 -0500

    add- title

--- src/Messi/Messi.js
+++ src/Messi/Messi.js
@@ -4,7 +4,14 @@ class Messi extends Component {
     render() {
         return (
 <>
+<section id="sixth-page">
+        <div class="sixth-bg"></div>
+        <div class="lightningg flashit"></div>
 
+        <div id="sixth-logo">
+        <h1>THE <span>🐐 GOAT</span></h1>
+        </div>
+</section>
 
 
 </>
